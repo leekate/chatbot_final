524ad7dd2187fe17a0e5a26de07bfb776db0f8b5
tmvld97 <tmvld97@naver.com>
2021-08-09 14:31:59 +0900

    08.09

--- models/ner/NerModel.py
+++ models/ner/NerModel.py
@@ -2,16 +2,15 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras import preprocessing
+from config.GlobalParams import HOS_SUBJECT, HOS_TYPE, HOS_TEL, HOS_LOC
 
-type = ['요양병원', '한방병원', '종합병원', '치과병원']
 # 개체명 인식 모델 모듈
 class NerModel:
     def __init__(self, model_name, proprocess):
 
         # BIO 태그 클래스 별 레이블
-        # self.index_to_ner = {1: 'O', 2: 'B_Hospital', 3: 'B_Location', 4: 'B_City', 5: 'B_Treat', 6: 'B_Tel', 7: 'B_Subject', 8: 'B_S_c', 9: 'B_Type', 0: 'PAD'}
-        self.index_to_ner = {1: 'O', 2: 'B_Hospital', 3: 'B_City', 4: 'B_Treat', 5: 'B_S_c', 6: 'B_Type', 0: 'PAD'}
-        # self.index_to_ner = {1: 'O', 2: 'B_City', 3: 'B_Treat', 4: 'B_S_c', 5: 'B_Type', 0: 'PAD'}
+        self.index_to_ner = {1: 'O', 2: 'B_Hospital', 3: 'B_City', 4: 'B_Treat', 5: 'B_S_c', 6: 'B_Type', 7: 'B_Subject', 8: 'B_Location', 9: 'B_Tel', 0: 'PAD'}
+
         # 의도 분류 모델 불러오기
         self.model = load_model(model_name)
 
@@ -38,8 +37,14 @@ class NerModel:
         tags = [self.index_to_ner[i] for i in predict_class.numpy()[0]]
 
         for i, key in enumerate(keywords) :
-            if key in type :
+            if key in HOS_TYPE :
                 tags[i] = 'B_Type'
+            elif key in HOS_SUBJECT :
+                tags[i] = 'B_Subject'
+            elif key in HOS_LOC :
+                tags[i] = 'B_Location'
+            elif key in HOS_TEL :
+                tags[i] = 'B_Tel'
         return list(zip(keywords, tags))
 
     def predict_tags(self, query):
@@ -59,27 +64,16 @@ class NerModel:
 
         np_predict = predict_class.numpy()[0]
         for i, key in enumerate(keywords) :
-            if key in type :
+            if key in HOS_TYPE :
                 np_predict[i] = 6
-
+            elif key in HOS_SUBJECT :
+                np_predict[i] = 7
+            elif key in HOS_LOC :
+                np_predict[i] = 8
+            elif key in HOS_TEL :
+                np_predict[i] = 9
 
         tags = []; count = 0
-        # for i, key in enumerate(keywords) :
-        #     if key in type :
-        #         predict_class.numpy()[0][i] = 6
-        #         print(predict_class.numpy()[0][i])
-        # print(predict_class.numpy()[0])
-        # for tag_idx in predict_class.numpy()[0]:
-        #     if tag_idx == 1: continue
-        #     elif tag_idx == 2 :
-        #         count += 1
-        #     if count < 2 :
-        #         tags.append(self.index_to_ner[tag_idx])
-        #
-        # if len(tags) == 0: return None
-        # if len(tags) != 1 and tags[-1] == 'B_Subject' :
-        #     del tags[-1]
-        # return tags
 
         for tag_idx in np_predict:
             if tag_idx == 1: continue
