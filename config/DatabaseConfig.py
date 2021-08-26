a8fbe21d2a430ac8fb1d043fcb35bae61f54222d
tmvld97 <tmvld97@naver.com>
2021-08-26 20:41:13 +0900

    fixed

--- config/DatabaseConfig.py
+++ config/DatabaseConfig.py
@@ -1,6 +1,9 @@
+"""
+자신 RDS 설정 값.
+"""
 DB_HOST = 'database-chatbot.cw2yedyk3t2l.ap-northeast-2.rds.amazonaws.com'
 DB_USER = 'admin'
-DB_PASSWORD = 'wnsrb1234!'
+DB_PASSWORD = '**********'
 DB_NAME = 'chatbotdb'
 DB_PORT = 3306
 
